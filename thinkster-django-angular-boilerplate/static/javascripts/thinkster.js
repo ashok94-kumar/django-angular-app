eb11bf67e78554760974736ca7594bfef80dabdf
devarashetty <marsai493@gmail.com>
2016-11-07 21:53:18 +0530

    added app to github

---
+++ thinkster-django-angular-boilerplate/static/javascripts/thinkster.js
@@ -0,0 +1,9 @@
+angular
+  .module('thinkster', [
+    'thinkster.config',
+    'thinkster.routes',
+    'thinkster.authentication',
+    'thinkster.layout',
+    'thinkster.posts',
+    'thinkster.profiles'
+  ]);
